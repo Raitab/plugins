d3385ed88bbaf8537efe80d2657f639c3936b758
Raitab <raitabplays@gmail.com>
2024-04-23 01:18:41 -0400

    Initial commit.

--- src/test/java/com/pricegraphopener/PriceGraphOpenerPluginTest.java
+++ src/test/java/com/pricegraphopener/PriceGraphOpenerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pricegraphopener;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PriceGraphOpenerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PriceGraphOpenerPlugin.class);
 		RuneLite.main(args);
 	}
 }
